2a9d99242ab0016414574f3953056c3730789bbc
voody2506 <voody2506@gmail.com>
2019-02-06 01:17:00 +0600

    123

--- ExampleProject/Assets/Scripts/PhotonMaster.cs
+++ ExampleProject/Assets/Scripts/PhotonMaster.cs
@@ -29,7 +29,7 @@ public class PhotonMaster : MonoBehaviourPunCallbacks
 
     roomOptions.MaxPlayers = 2;
 
-    connectStatus.text = "Connected";
+    connectStatus.text = "Connected to master";
     connectStatus.color = new Color(58.0f/255.0f, 255.0f/255.0f, 4.0f/255.0f);
     connected = true;
 
